dd8a7527415f5561e046cccde3af3088dcaeda86
Omar Attallah <attallah.dev@gmail.com>
2023-05-16 00:10:54 +0300

    chore: export all component from components folder

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Label } from './Label/Label';
+export { default as Card } from './cards/BasicCard/Card';
+export { default as CountryCard } from './cards/CountryCard/CountryCard';
+export { default as Button } from './Button/Button';
