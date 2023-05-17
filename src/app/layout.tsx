8223ad8913ddb186cb2ee35b01a851cc3cb187f7
Omar Attallah <attallah.dev@gmail.com>
2023-05-17 22:06:35 +0300

    chore: add Meta tags to improve application SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,13 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
           content="A simple app to view countries and their details"
           key="desc"
         />
+        <meta property="og:title" content="Countries App" />
+        <meta
+          property="og:description"
+          content="A simple app to view countries and their details"
+        />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ErrorBoundary>
         <main className="main-layout">
